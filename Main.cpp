0406194878ae48d6c76fd28e69d4bc4ebe7f71a8
shubham <shubhamd6800@gmail.com>
2024-01-13 16:17:59 +0530

    final commit

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,19 @@
-#include<iostream>
-#include"Functionalities.h"
+#include <iostream>
+#include <thread>
+#include "Functionalities.h"
+#include <array>
 
 int main()
 
 {
     Container data;
-    CreateObjects(data);
-    CalculateTaxPayable(data);
-    CallParenOperator(data);
+    std::array<std::thread, 3> arr;
+
+    arr[0] = std::thread(CreateObjects, std::ref(data));
+    arr[0].join();
+
+    arr[1] = std::thread(CalculateTaxPayable, std::ref(data));
+    arr[2] = std::thread(CallParenOperator, std::ref(data));
+    arr[1].join();
+    arr[2].join();
 }
